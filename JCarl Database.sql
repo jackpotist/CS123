fdc8b3ed08e48f0aeaef65f60062a7fd1e584e3f
JMSantos <johnmichaelsantos1681@yahoo.com>
2014-09-23 01:19:08 +0800

    Added Employee Table

--- JCarl Database.sql
+++ JCarl Database.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS suppliers
  supplier_name VARCHAR(254) NOT NULL,
  category VARCHAR(254) NOT NULL,
  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id));
+ 
+CREATE TABLE IF NOT EXISTS employee
+(employee_id INTEGER(6) NOT NULL,
+ employee_name VARCHAR(254) NOT NULL,
+ pay_Rate INTEGER(8) NOT NULL,
+ CONSTRAINT employee_pk PRIMARY KEY (employee_id));
