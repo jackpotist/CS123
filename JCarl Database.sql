4094f72ffbdcb6022d79f7a62daea2f35667fdf9
Pia Encarnacion <piaencar@hotmail.com>
2014-09-23 01:09:12 +0800

    Added Material Table

--- JCarl Database.sql
+++ JCarl Database.sql
@@ -7,3 +7,10 @@ CREATE TABLE IF NOT EXISTS suppliers
  supplier_name VARCHAR(254) NOT NULL,
  category VARCHAR(254) NOT NULL,
  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id));
+ 
+ CREATE TABLE IF NOT EXISTS material
+(material_id INTEGER(6) NOT NULL,
+ material_name VARCHAR(254) NOT NULL,
+ quantity INTEGER(45) NOT NULL,
+ price DOUBLE(10,4) NOT NULL,
+ CONSTRAINT material_pk PRIMARY KEY (material_id));
