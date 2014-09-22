24fb1591d06075aedcf99ef440a25140b3cd95ba
Pia Encarnacion <piaencar@hotmail.com>
2014-09-23 03:04:59 +0800

    Added purchase order and projects tables

--- JCarl Database.sql
+++ JCarl Database.sql
@@ -8,11 +8,11 @@ CREATE TABLE IF NOT EXISTS suppliers
  category VARCHAR(254) NOT NULL,
  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id));
  
-CREATE TABLE IF NOT EXISTS employee
+CREATE TABLE IF NOT EXISTS employees
 (employee_id INTEGER(6) NOT NULL,
  employee_name VARCHAR(254) NOT NULL,
  pay_Rate INTEGER(8) NOT NULL,
- CONSTRAINT employee_pk PRIMARY KEY (employee_id));
+ CONSTRAINT employees_pk PRIMARY KEY (employee_id));
 
 CREATE TABLE IF NOT EXISTS materials
 (material_id INTEGER(6) NOT NULL,
@@ -20,3 +20,19 @@ CREATE TABLE IF NOT EXISTS materials
  quantity INTEGER(45) NOT NULL,
  price DOUBLE(10,4) NOT NULL,
  CONSTRAINT materials_pk PRIMARY KEY (material_id));
+ 
+CREATE TABLE IF NOT EXISTS po
+(po_id INTEGER(6) NOT NULL,
+ project_name VARCHAR(254) NOT NULL,
+ date_issued INTEGER(8) NOT NULL,
+ total_price DOUBLE(10,4) NOT NULL,
+ CONSTRAINT po_pk PRIMARY KEY (po_id),
+ CONSTRAINT po_fk FOREIGN KEY (project_name) REFERENCES projects(project_name));
+ 
+CREATE TABLE IF NOT EXISTS projects
+(project_id INTEGER(6) NOT NULL,
+ project_name VARCHAR(254) NOT NULL,
+ start_date INTEGER(8) NOT NULL,
+ end_date INTEGER(8) NOT NULL,
+ client VARCHAR(254) NOT NULL,
+ CONSTRAINT projects_pk PRIMARY KEY (project_id));
