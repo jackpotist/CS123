271af18d9a773a14bc3b2249a190673f6dfc4bcd
Pia Encarnacion <piaencar@hotmail.com>
2014-09-23 01:44:57 +0800

    Merge branch 'piaencar' of https://github.com/jackpotist/CS123 into jsantos16
    Conflicts:
    	JCarl Database.sql

--- JCarl Database.sql
+++ JCarl Database.sql
@@ -13,3 +13,10 @@ CREATE TABLE IF NOT EXISTS employee
  employee_name VARCHAR(254) NOT NULL,
  pay_Rate INTEGER(8) NOT NULL,
  CONSTRAINT employee_pk PRIMARY KEY (employee_id));
+
+CREATE TABLE IF NOT EXISTS materials
+(material_id INTEGER(6) NOT NULL,
+ material_name VARCHAR(254) NOT NULL,
+ quantity INTEGER(45) NOT NULL,
+ price DOUBLE(10,4) NOT NULL,
+ CONSTRAINT materials_pk PRIMARY KEY (material_id));
