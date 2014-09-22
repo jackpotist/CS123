70963911c7f25aa51cf93c634c5d9fde24c4dbc2
javyflavier <javyflavier@yahoo.com>
2014-09-23 01:14:15 +0800

    Material Table Edit
    Follow format of the data dictionary

--- JCarl Database.sql
+++ JCarl Database.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS suppliers
  category VARCHAR(254) NOT NULL,
  CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id));
  
- CREATE TABLE IF NOT EXISTS material
+ CREATE TABLE IF NOT EXISTS materials
 (material_id INTEGER(6) NOT NULL,
  material_name VARCHAR(254) NOT NULL,
  quantity INTEGER(45) NOT NULL,
  price DOUBLE(10,4) NOT NULL,
- CONSTRAINT material_pk PRIMARY KEY (material_id));
+ CONSTRAINT materials_pk PRIMARY KEY (material_id));
